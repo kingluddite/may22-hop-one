9e716f7d04b1533e5f52634bd86d87ad6adf70d8
philip howley <howley.phil@gmail.com>
2022-05-11 21:58:30 -0400

    Add destructuring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Header from "./components/Header"
+
 function App() {
   return (
     <div className='container'>
+      <Header age='24' likesSoccer='true' />
       <h1>hello world</h1>
       <h2>hello again</h2>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header({ age, likesSoccer }) {
+  return (
+    <div>
+      Phil just turned {age} {likesSoccer}
+    </div>
+  )
+}
+
+export default Header
